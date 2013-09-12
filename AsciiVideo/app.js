c7de70292bdd99508534677c3b5dedfd925dab92
nikolay-spasov <nikolay.spasov.bg@gmail.com>
2013-09-13 00:53:52 +0300

    commit new version

--- AsciiVideo/app.js
+++ AsciiVideo/app.js
@@ -4,19 +4,18 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+//var routes = require('./routes');
 var user = require('./routes/user');
+var stream_file = require('./routes/stream_file');
 var http = require('http');
 var path = require('path');
-var stream_file = require('./routes/stream_file');
-var fs = require('fs');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,14 +28,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', routes.index);
-//app.get('/users', user.list);
 app.get('/', function(req, res) {
-   //res.writeHead('Content-Type', 'text/html');
-   res.render('AsciiVideo');
+  res.render('AsciiVideo');
 });
 app.post('/requestVideo', stream_file.request_video);
-app.get('/streamFile/:id', stream_file.stream_file);
+app.post('/streamFile/:id', stream_file.stream_file);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- AsciiVideo/routes/stream_file.js
+++ AsciiVideo/routes/stream_file.js
@@ -2,26 +2,15 @@ var request = require('request');
 var src = "http://www.w3schools.com/tags/movie.mp4";
 
 var sources = [];
-sources.push({'src': src });
-
-/*
-exports.stream_file = function(req, res){
-    var index = streamSources.push({ 'src': req.body.videoSource }) - 1;
-    res.writeHead('Content-Type', 'application/json');
-    res.end({ 'id': index });
-};*/
+sources.push({ 'src': src });
 
 exports.stream_file = function(req, res) {
-    var id = parseInt(req.params.id);
-    console.log(sources[id]);
-    request.get(sources[id].src).pipe(res);
+  var id = parseInt(req.params.id);
+  request.get(source[id].src).pipe(res);
 };
 
 exports.request_video = function(req, res) {
-    var remoteSrc = req.body.remoteSrc;
-    console.log(req.body);
-    var index = sources.push({'src': remoteSrc}) - 1;
-    res.json({
-    	id: index
-    });
+  var remoteSrc = req.body.remoteSrc;
+  var index = sources.push( { 'src': remoteSrc }) - 1;
+  res.json({ id: index });
 };
