42289f685e4e452519daa57d92310a8fb086c21d
nikolay-spasov <nikolay.spasov.bg@gmail.com>
2013-09-18 12:00:32 +0300

    initial GithubVersion

--- AsciiVideo/public/javascripts/asciiVideo.js
+++ AsciiVideo/public/javascripts/asciiVideo.js
@@ -1,59 +1,31 @@
-﻿
-var video
-  , videoSources = [
-      'test.mp4',
-      'test2.webm',
-      'defragUndead.webm',
-      'starCraft.webm',
-      'keyGenDance.webm'
-  ]
-  , characters = "*?&AXRSabcdefghijklmnopqrstuvwxyz".split("")
+﻿var video
+  , characters = "*?&AXRSabcdefghijklnopqrstuvwxyz1234567890".split("")
   , ctx1
   , ctx2
   , offScreenCanvas
   , offScreenCtx
   , renderer = offscreenRendering
   , colorFunc = getColor
-  , canvasWidth = 160
-  , canvasHeight = 120
-  , imageData
-  , x
-  , y
-  , offset
-  , color
+  , canvasWidth = 120
+  , canvasHeight = 90
   , brightness
-  , character;
+  , showFps;
 
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-var constraints = { audio: false, video: true };
-
-function userMediaSuccess(stream) {
-    video.stream = stream;
-    if (window.URL) {
-        video.src = window.URL.createObjectURL(stream);
-    } else {
-        video.src = stream;
-    }
-    video.play();
-}
-
-function userMediaError(error) {
-    console.log(error);
-}
-
-$(document).ready(function () {
+window.onload = function () {
     video = document.createElement('video');
     ctx1 = document.createElement('canvas').getContext('2d');
     ctx2 = document.getElementById('c2').getContext('2d');
     ctx2.fillStyle = "#000";
 
     offScreenCanvas = document.createElement('canvas');
-    offScreenCanvas.width = 970;
-    offScreenCanvas.height = 640;
+    offScreenCanvas.width = 845;
+    offScreenCanvas.height = 450;
     offScreenCtx = offScreenCanvas.getContext('2d');
     offScreenCtx.font = "10px Verdana";
+    offScreenCtx.baseline = 'top';
     
     var selectColorScale = document.getElementById('color');
     selectColorScale.addEventListener('change', function (evt) {
@@ -76,24 +48,34 @@ $(document).ready(function () {
         }
     }, false);
 
-    var btnPlay = document.getElementById('btn-play');
-    var btnPause = document.getElementById('btn-pause');
-
-    video.addEventListener('play', function () {
+    video.addEventListener('play', function() {
         timerCallback();
     }, false);
 
-    btnPlay.addEventListener('click', function () {
-        video.src = "/defragUndead.webm";
+    document.getElementById('btn-play').addEventListener('click', function () {
+        if (!video.src) video.src = "/defragUndead.webm";
         video.play();
     }, false);
 
-    btnPause.addEventListener('click', function () {
+    document.getElementById('btn-pause').addEventListener('click', function () {
         video.pause();
     }, false);
 
     document.getElementById('btn-camera-record').addEventListener('click', function () {
-        navigator.getUserMedia(constraints, userMediaSuccess, userMediaError);
+        navigator.getUserMedia({ 
+            'audio': false, 
+            'video': true 
+        }, function(stream) {
+            video.stream = stream;
+            if (window.URL) {
+                video.src = window.URL.createObjectURL(stream);
+            } else {
+                video.src = stream;
+            }
+            video.play();
+        }, function(error) {
+            console.log(error);
+        });
     }, false);
 
     document.getElementById('btn-take-photo').addEventListener('click', function () {
@@ -106,90 +88,83 @@ $(document).ready(function () {
         document.getElementById('controls').appendChild(downloadLink);
     }, false);
     
-    $('#btn-play-remote-video').on('click', function() {
-	$.ajax({
-	    type: 'POST',
-	    url: '/requestVideo',
-	    dataType: 'json',
-	    data: { 'remoteSrc': $('#txt-src').val() }
-	}).done(function(data) {
-	    video.src = "/streamFile/" + data.id;
-	    setTimeout(function() {
-		video.play();
-	    }, 2000);
-	});
-    });
-});
+    document.getElementById('btn-play-remote-video').addEventListener('click', function() {
+        $.ajax({
+            type: 'POST',
+            url: '/requestVideo',
+            dataType: 'json',
+            data: { 'remoteSrc': $('#txt-src').val() }
+        }).done(function(data) {
+            video.src = "/streamFile/" + data.id;
+            setTimeout(function() {
+                video.play();
+            }, 2000);
+        });
+    }, false);
+    
+    showFps = document.getElementById('show-fps');
+};
 
 function timerCallback() {
     if (video.paused || video.ended) return;
-
+    
+    var start = new Date();
     renderer();
-
+    var end = new Date() - start;
+    showFps.innerHTML = (1000 / end).toFixed(2);
+    
     setTimeout(function () {
         timerCallback();
-    }, 0);
+    }, 4);
 }
 
 function offscreenRendering() {
-    ctx1.drawImage(video, 0, 0, 120, 90);
+    var colorsSum
+      , avg
+      , imageData
+      , offset
+      , color
+      , x, y;
+      
+    ctx1.drawImage(video, 0, 0, canvasWidth, canvasHeight);
     imageData = ctx1.getImageData(0, 0, canvasWidth, canvasHeight);
-
-    var c;
-    
     ctx2.fillRect(0, 0, 845, 450);
-
-    var frames = {};
     offScreenCtx.clearRect(0, 0, offScreenCanvas.width, offScreenCanvas.height);
-    //var d = new Date();
+    
     for (y = 0; y < canvasHeight; y += 2) {
         for (x = 0; x < canvasWidth; x++) {
             offset = (y * canvasWidth + x) * 4;
-
+            
             color = getColorAtOffset(imageData.data, offset);
-
-            if ((color.red + color.green + color.blue) < 50) continue;
-
-            var contrastedColor = color;
-
-            brightness = (0.299 * contrastedColor.red + 0.587 * contrastedColor.green + 0.114 * contrastedColor.blue) / 255;
-
-            character = characters[(characters.length - 1) - Math.round(brightness * (characters.length - 1))];
-
-            c = colorFunc(color.red, color.green, color.blue);
-            if (!frames[c]) {
-                frames[c] = [];
-            }
-            frames[c].push({ row: y * 5, col: x * 7, char: character });
+            colorsSum = color.red + color.green + color.blue;
+            avg = Math.round(colorsSum / 3);
+            
+            character = getCharacter(getBrightness(color.red, color.green, color.blue));
+            
+            offScreenCtx.fillStyle = colorFunc(color.red, color.green, color.blue, avg);
+            offScreenCtx.fillText(character, x * 7, y * 5);
         }
     }
-
-    offScreenCtx.clearRect(0, 0, 845, 450);
-    
-    var key, i;
-    for (key in frames) {
-        offScreenCtx.strokeStyle = key;
-        for (i = 0; i < frames[key].length; i++) {
-            offScreenCtx.strokeText(frames[key][i].char, frames[key][i].col, frames[key][i].row);
-        }
-    }
-
     ctx2.drawImage(offScreenCanvas, 0, 0);
 }
 
-function getColorAsGreenScale(r, g, b) {
-    var avg = Math.round((r + g + b) / 3);
-
-    return getColor(avg, avg * 2, avg);
+function getBrightness(r, g, b) {
+    return (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 }
 
-function getColorAsGrayScale(r, g, b) {
-    var avg = Math.round((r + g + b) / 3);
+function getCharacter(brightness) {
+    return characters[(characters.length - 1) - Math.round(brightness * (characters.length - 1))];
+}
 
+function getColorAsGreenScale(r, g, b, avg) {
+    return getColor(avg, avg << 1, avg);
+}
+
+function getColorAsGrayScale(r, g, b, avg) {
     return getColor(avg, avg, avg);
 }
 
-function getColor(r, g, b) {
+function getColor(r, g, b, avg) {
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
